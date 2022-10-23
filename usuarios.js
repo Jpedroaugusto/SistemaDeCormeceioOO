ebe54f59181c3b6659cd8563d09fdcfb15f9b915
Jpedroaugusto <pedroagusuto131204@gmail.com>
2022-10-23 05:23:52 -0300

    atualização da tabela e novas funções

--- usuarios.js
+++ usuarios.js
@@ -1,18 +1,22 @@
-// CLASSE USUÁRIO
 class Usuario {
-    // MÉTODO CONSTRUTOR
-    constructor() {
-    }
-    
-    // MÉTODO PARA CADASTRAR USUÁRIOS
+    constructor() {}
     cadastrarUsuario(nome,cpf,email) {
+        let busca = this.nomeFormatado(nome)
         let usuario = {
             nome: nome,
             cpf: cpf,
-            email: email   
+            email: email,   
+            busca: busca
         }
 
         return usuario;
+    }
+
+    nomeFormatado(nome) {
+        let nomeBusca = nome.split(" ").join("");
+        nomeBusca = nomeBusca.toLowerCase();
+        nomeBusca = nomeBusca.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, '');
+        return nomeBusca
     }
     
     validarCampos(nome,cpf,email) {
@@ -22,33 +26,44 @@ class Usuario {
             return false;
         }
     }
-    
-    
 }
 
-// CLASSE LOCAL STORAGE
 class LocalDb {
-    constructor() {
-        
-    }
-    
-    registrar(userDb) {
-        let id = localStorage.length;
-        id++;
-        localStorage.setItem(id, JSON.stringify(userDb));
-    }
-    
+    constructor() {}
 
-    atulizar(id,nome,cpf,email) {
+    registrar(userDb) {
+        localStorage.setItem(userDb.cpf, JSON.stringify(userDb));
+    }
+
+    atulizar(chave,nome,email) {
+        let busca = this.nomeFormatado(nome)
         if(nome != "") {
             let usuario = {
                 nome: nome,
-                cpf: cpf,
-                email: email
+                cpf: chave,
+                email: email,
+                busca: busca
             }
-            
-            localStorage.setItem(id, JSON.stringify(usuario));
+            localStorage.setItem(chave, JSON.stringify(usuario));
         }
+    }
+
+    nomeFormatado(nome) {
+        let nomeBusca = nome.split(" ").join("");
+        nomeBusca = nomeBusca.toLowerCase();
+        nomeBusca = nomeBusca.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, '');
+        return nomeBusca
+    }
+    
+    pesquisar(pesquisa) {
+        let registros = Array()
+        let busca = this.nomeFormatado(pesquisa)
+        for(let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let usuario = JSON.parse(localStorage.getItem(key));
+            registros.push(usuario);
+        }
+        return registros.filter(function(i) { return i.busca == busca})
     }
 
     deletar(id) {
@@ -58,33 +73,59 @@ class LocalDb {
 
 
 // MOSTRANDO O VALOR DA TABELA QUANDO A PÁGINA É CARREGADA
-window.addEventListener('load', mostrarTabela);
+window.addEventListener('load', load);
 
-
-// SETANDO ATRIBUTOS AOS BOTÕES PRINCIPAIS
-document.getElementById('btn-cancelar-cadastro').setAttribute("onclick","limparCampos('cadastro')");
-document.getElementById('btn-cadastrar').setAttribute("onclick","cadastrar()");
-
-document.getElementById('btn-cancelar-atualizar').setAttribute("onclick","limparCampos('atualizar')");
-document.getElementById('btn-atualizar').setAttribute("onclick", `setAtualizar()`);
+function load() {
+    // SETANDO ATRIBUTOS PRINCIPAIS
+    document.getElementById('btn-cancelar-cadastro').setAttribute("onclick","limparCampos('cadastro')");
+    document.getElementById('btn-cadastrar').setAttribute("onclick","cadastrar()");
+    document.getElementById('btn-cancelar-atualizar').setAttribute("onclick","limparCampos('atualizar')");
+    document.getElementById('btn-atualizar').setAttribute("onclick", `setAtualizar()`);
+    document.getElementById("field-pesquisar").addEventListener("keyup", pesquisar)
+    
+    
+    mostrarTabela('null');
+}
 
 // CRIAÇÃO DOS OBJETOS
 let usuario = new Usuario();
 let localDb = new LocalDb();
 
+function mostrarTabela(valor,usuarioFiltrado){
+    let tr = "";
+    if(valor != 'pesquisa') {
+        for(let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let usuario = JSON.parse(localStorage.getItem(key));
+            tr += `<tr onclick="atualizar(${key})">`;
+            tr += `<td>${i+1}</td>`;
+            tr += `<td>${usuario.nome}</td>`;
+            tr += `<td>${usuario.cpf}</td>`;
+            tr += `<td>${usuario.email}</td>`;
+            tr += `</tr>`;
+        }
+    } else {
+        let usuario = localDb.pesquisar(usuarioFiltrado);
+        for(let i = 0; i < usuario.length; i++) {
+            tr += `<tr onclick="atualizar(${usuario[i].cpf})">`;
+            tr += `<td>${usuario[i].nome}</td>`;
+            tr += `<td>${usuario[i].cpf}</td>`;
+            tr += `<td>${usuario[i].email}</td>`;
+            tr += `</tr>`;
+        }
+    }
+    document.getElementById('table').innerHTML = tr;
+}
+
+
+// FUNÇÔES DE DECISÕES
 function cadastrar() {
-
     let cadastro = getCamposCadastro()
-
-    
     if(usuario.validarCampos(cadastro.fieldNome, cadastro.fieldCPF, cadastro.fieldEmail) == true) {
-        
         let userDb = usuario.cadastrarUsuario(cadastro.fieldNome, cadastro.fieldCPF, cadastro.fieldEmail)
         localDb.registrar(userDb);
-        
-        limparCampos();
-        location.reload();
-
+        mostrarTabela()
+        limparCampos('cadastro');
     } else {
         if(cadastro.fieldNome == "") {
             document.getElementById('alert-cadastro').innerHTML = `Prencha o seu nome`;
@@ -98,24 +139,98 @@ function cadastrar() {
     }
 }
 
-
-
 function getCamposCadastro() {
     let cadastro = {
         fieldNome: document.getElementById('field-nome-cadastro').value,
         fieldCPF: document.getElementById('field-cpf-cadastro').value,
         fieldEmail: document.getElementById('field-email-cadastro').value
     }
-    
     return cadastro;
 }
 
+
+
+
+
+function atualizar(chave) {
+    console.log(chave)
+    document.getElementById('btn-cancelar-atualizar').setAttribute("onclick", `cancelar(${chave})`);
+    document.getElementById('btn-deletar-atualizar').setAttribute("onclick", `deletar(${chave})`);
+    document.getElementById('btn-atualizar').setAttribute("onclick", `setAtualizar(${chave})`);
+    
+    limparCampos('atualizar')
+
+    let usuario = JSON.parse(localStorage.getItem(chave));
+    
+    document.getElementById('painel-usuario').innerHTML = ``;
+    document.getElementById('painel-usuario').innerHTML += `Nome de usuario: <strong>${usuario.nome}</strong><br>`;
+    document.getElementById('painel-usuario').innerHTML += `CPF: <strong>${usuario.cpf}</strong><br>`;
+    document.getElementById('painel-usuario').innerHTML += `Email: <strong>${usuario.email}</strong><br><br>`;
+}
+
+function setAtualizar(chave) {
+    let atualizar = getCamposAtualizacao()
+    let usuarioS = JSON.parse(localStorage.getItem(chave));
+    
+    if(atualizar.fieldNome != "" && atualizar.fieldEmail != "") {
+        localDb.atulizar(chave, atualizar.fieldNome, atualizar.fieldEmail);
+        cancelar();
+    }   
+    else if(atualizar.fieldNome != "" && atualizar.fieldEmail == "") {
+        localDb.atulizar(chave, atualizar.fieldNome, usuarioS.email);
+        cancelar();
+    }
+    else if(atualizar.fieldEmail != "" && atualizar.fieldNome == "") {
+        localDb.atulizar(chave, usuarioS.nome, atualizar.fieldEmail);
+        cancelar();
+    }
+    if(atualizar.fieldNome == "" && atualizar.fieldEmail == "") {
+        document.getElementById('alert-atualizar').innerHTML = 'Preencha um campo';
+    }
+    if(chave == null) {
+        document.getElementById('alert-atualizar').innerHTML = 'Escolha um item da tabela';
+    }
+    mostrarTabela('null')
+}
+
+
+function pesquisar() {
+    if(document.getElementById("field-pesquisar").value != "") {
+        mostrarTabela('pesquisa',document.getElementById("field-pesquisar").value);
+    } else {
+        mostrarTabela('null')
+    }
+}
+
+function deletar(chave) {
+    
+    if(chave != "") {
+        localDb.deletar(chave);
+        limparCampos('atualizar')
+        mostrarTabela('null')
+        cancelar(chave);
+    } else {
+        document.getElementById('alert-atualizar').innerHTML = 'Escolha um item da tabela';
+    }
+}
+
+function cancelar(chave) {
+    document.getElementById('btn-atualizar').removeAttribute("onclick", `setAtualizar(${chave})`);
+    document.getElementById('btn-deletar-atualizar').removeAttribute("onclick", `deletar(${chave})`);
+    
+    document.getElementById('painel-usuario').innerHTML = "";
+    document.getElementById('alert-atualizar').innerHTML = "";
+    
+    limparCampos('atualizar');
+}
+
+
+// FUNÇÕES DE CAMPOS
 function getCamposAtualizacao() {
     let atualizacao = {
         fieldNome: document.getElementById('field-nome-atualizar').value,
         fieldEmail: document.getElementById('field-email-atualizar').value
     }
-
     return atualizacao;
 }
 
@@ -127,95 +242,11 @@ function limparCampos(secao) {
 
         document.getElementById('alert-cadastro').innerHTML = "";
     }
+
     else if(secao = 'atualizar') {
         document.getElementById('field-nome-atualizar').value = "";
         document.getElementById('field-email-atualizar').value = "";
-
         document.getElementById('painel-usuario').innerHTML = "";
         document.getElementById('alert-atualizar').innerHTML = "";
-    }
-}
-
-
-
-function mostrarTabela(){
-    let table = document.getElementById('table');
-    let tr = "";
-    
-    
-    for(let i = 1; i <= localStorage.length; i++) {
-        let usuario = JSON.parse(localStorage.getItem(i));
-        
-        tr += `<tr onclick="atualizar(${i})">`;
-        tr += `<td>${i}</td>`;
-        tr += `<td>${usuario.nome}</td>`;
-        tr += `<td>${usuario.cpf}</td>`;
-        tr += `<td>${usuario.email}</td>`;
-        tr += `</tr>`;
-    }
-    table.innerHTML += tr;
-}
-
-function atualizar(i) {
-    document.getElementById('btn-cancelar-atualizar').setAttribute("onclick", `cancelar(${i})`);
-    document.getElementById('btn-deletar-atualizar').setAttribute("onclick", `deletar(${i})`);
-    document.getElementById('btn-atualizar').setAttribute("onclick", `setAtualizar(${i})`);
-    
-    document.getElementById('alert-atualizar').innerHTML = "";
-    document.getElementById('painel-usuario').innerHTML = "";
-
-    let painelDiv = document.getElementById('painel-usuario');
-    let usuario = JSON.parse(localStorage.getItem(i));
-    
-    painelDiv.innerHTML = ``;
-    painelDiv.innerHTML += `Nome de usuario: <strong>${usuario.nome}</strong><br>`;
-    painelDiv.innerHTML += `CPF: <strong>${usuario.cpf}</strong><br>`;
-    painelDiv.innerHTML += `Email: <strong>${usuario.email}</strong><br><br>`;
-}
-
-function setAtualizar(i) {
-    let atualizar = getCamposAtualizacao()
-
-    let usuarioS = JSON.parse(localStorage.getItem(i));
-    
-    if(atualizar.fieldNome != "" && atualizar.fieldEmail != "") {
-        localDb.atulizar(i, atualizar.fieldNome, usuarioS.cpf, atualizar.fieldEmail);
-        location.reload();
-    }
-    else if(atualizar.fieldNome != "") {
-        localDb.atulizar(i, atualizar.fieldNome, usuarioS.cpf, usuarioS.nome);
-        location.reload();
-    }
-    else if(atualizar.fieldEmail != "") {
-        localDb.atulizar(i, usuarioS.nome, usuarioS.cpf, atualizar.fieldEmail);
-        location.reload();
-    }
-    
-    if(atualizar.fieldNome == "" && atualizar.fieldEmail == "") {
-        document.getElementById('alert-atualizar').innerHTML = 'Preencha um campo';
-    }
-
-    console.log(i)
-    if(i == null) {
-        document.getElementById('alert-atualizar').innerHTML = 'Escolha um item da tabela';
-    }
-}
-
-function cancelar(i) {
-    document.getElementById('btn-atualizar').removeAttribute("onclick", `setAtualizar(${i})`);
-    document.getElementById('btn-deletar-atualizar').removeAttribute("onclick", `deletar(${i})`);
-    
-    document.getElementById('painel-usuario').innerHTML = "";
-    document.getElementById('alert-atualizar').innerHTML = "";
-
-    limparCampos(atualizar);
-}
-
-function deletar(i) {
-    if(i == localStorage.length){
-        localDb.deletar(i);
-        location.reload();
-    } else {
-        document.getElementById('alert-atualizar').innerHTML = 'Apague o último item da tabela';
     }
 }
